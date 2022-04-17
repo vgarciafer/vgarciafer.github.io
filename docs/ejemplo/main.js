c17739bb59ff6ceb4e897ded4d595b884c6ec34a
vgarciafer <92395911+vgarciafer@users.noreply.github.com>
2022-04-18 00:52:05 +0200

    Update main.js

--- docs/ejemplo/main.js
+++ docs/ejemplo/main.js
@@ -18,8 +18,8 @@ var width = 500,
       .range([40, r]);
 
     var svg = d3.select("#chart").append("svg")
-      .attr("width", width + margin.right + margin.left)
-      .attr("height", height + margin.left + margin.right)
+      .attr("width", width)
+      .attr("height", height)
       .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
