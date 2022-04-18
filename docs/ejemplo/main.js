7d809b134c261df7c442baf07244d5dfa55b09fb
vgarciafer <92395911+vgarciafer@users.noreply.github.com>
2022-04-18 20:08:13 +0200

    Update main.js

--- docs/ejemplo/main.js
+++ docs/ejemplo/main.js
@@ -26,7 +26,7 @@ function procesaEspiral(json){
       start = 0,
       end = 2.25,
       numSpirals = 6; 
-      margin = {top:50,bottom:50,left:auto,right:auto};
+      margin = {top:50,bottom:50,left:50,right:50};
 
     var theta = function(r) {
       return numSpirals * Math.PI * r;
