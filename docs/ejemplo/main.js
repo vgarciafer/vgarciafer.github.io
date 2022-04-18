34c15683b85b459eab5ff79ef15ebcb18a8ae14f
vgarciafer <92395911+vgarciafer@users.noreply.github.com>
2022-04-18 13:29:00 +0200

    Update main.js

--- docs/ejemplo/main.js
+++ docs/ejemplo/main.js
@@ -3,8 +3,10 @@ var rccdata = [];
 
 function processJSON(json){
   for(let i = 0; i < json.length; i++){
-    rccdata.push(json[i])
+    rccdata.push(json[i]);
+    alert(rccdata.length);
   }
+  
 }
 fetch('https://data.cdc.gov/resource/9mfq-cb36.json?state=AL')
   .then(response => response.json())
@@ -49,7 +51,6 @@ var width = 500,
       .style("fill", "none")
       .style("stroke", "steelblue");
 
-    alert(rccdata.length);
     var spiralLength = path.node().getTotalLength(),
         N = rccdata.length,
         barWidth = (spiralLength / N) - 1;
